634fe502c40135e1747de24dd5a5adbcb2927305
noelpc03 <noel2692003@gmail.com>
2025-01-31 16:40:55 -0500

    refactor: improve code formatting and enhance GUI layout for expression evaluator

--- Proyecto final/src/GUI.hs
+++ Proyecto final/src/GUI.hs
@@ -5,22 +5,40 @@ import Graphics.UI.Threepenny.Core
 import Evaluator (eval)
 import Parser (parseExpr)
 import Text.Parsec (parse)
+import Data.List (isInfixOf)
 
 main :: IO ()
 main = startGUI defaultConfig setup
 
 setup :: Window -> UI ()
 setup window = do
-    return window # set title "My Application"
-    button <- UI.button # set text "Click me"
-    input <- UI.input
+    return window # set title "Evaluador de Expresiones Matemáticas"
+    
+    -- Crear elementos de la interfaz
+    title <- UI.h1 # set text "Evaluador de Expresiones Matemáticas"
+    input <- UI.input # set (attr "placeholder") "Introduce una expresión"
+    button <- UI.button # set text "Evaluar"
     output <- UI.span
-    layout <- UI.div #+ [element button, element input, element output]
+    errorMsg <- UI.div # set style [("color", "red")]
+    
+    -- Crear el layout
+    layout <- UI.div #. "container" #+ 
+        [ element title
+        , UI.div #. "input-group" #+ [element input, element button]
+        , UI.div #. "output-group" #+ [element output, element errorMsg]
+        ]
+    
     getBody window #+ [element layout]
-
+    
+    -- Manejar eventos
     on UI.click button $ \_ -> do
-        expr <- get UI.value input
+        expr <- get value input
         let resultado = case parse parseExpr "" expr of
                           Left _ -> "Error de sintaxis"
                           Right e -> "Resultado: " ++ show (eval e)
-        element output # set UI.text resultado
+        element output # set text resultado
+        element errorMsg # set text (if "Error" `isInfixOf` resultado then resultado else "")
+    
+    -- Agregar estilos
+    UI.addStyleSheet window "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    return ()
